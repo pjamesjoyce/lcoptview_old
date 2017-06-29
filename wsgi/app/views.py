2b83a2d454634a4e2ca5207c56ddff1744dbcc87
James Joyce <pjamesjoyce@gmail.com>
2017-06-29 12:47:33 +0200

    first templates

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,5 +1,8 @@
 from app import app
+from flask import render_template
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Flask World!"
+	#return "Hello, Flask World!"
+	return render_template('test.html')
