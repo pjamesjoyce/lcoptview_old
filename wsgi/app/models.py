353123955259a368de0528f8ddd1a3195eccb6cf
James Joyce <pjamesjoyce@gmail.com>
2017-06-30 15:07:22 +0200

    first database test on openshift

--- wsgi/app/models.py
+++ wsgi/app/models.py
@@ -1,5 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-
 db = SQLAlchemy(app)
+
+
+class ModelFile(db.Model):
+    __tablename__ = 'modelfiles'
+    id = db.Column('modelfile_id', db.Integer, primary_key=True)
+    filename = db.Column(db.String(255))
+    filepath = db.Column(db.String(255))
+
+    def __init__(self, filename, filepath):
+        self.filename = filename
+        self.filepath = filepath
